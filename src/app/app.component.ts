1b810b5f8361f9d0279ae812af2519be3818bce4
victor <gfeng005@uottawa.ca>
2025-02-19 11:09:30 -0500

    initialized the project

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatSliderModule} from '@angular/material/slider';
+
+@Component({
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    MatSliderModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'course-registration';
+}

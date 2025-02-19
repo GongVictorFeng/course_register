637183454f610601347f94ea3765d2c9d59e63c4
victor <gfeng005@uottawa.ca>
2025-02-19 15:08:44 -0500

    created a sidenav and a toolbor to open the side nave

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import {  MatListModule } from "@angular/material/list";
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button'
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    MatSliderModule
+    MatSliderModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'

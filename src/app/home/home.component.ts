9a34b6e00ec7d83d50c09fea1c0bab3959abc8b6
victor <gfeng005@uottawa.ca>
2025-02-19 22:07:16 -0500

    updated home component to display all course cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
+import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
+import { map, Observable } from 'rxjs';
+import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   imports: [
         MatIconModule,
         MatButtonModule,
-        MatTabsModule
+        MatTabsModule,
+        CommonModule,
+        CoursesCardListComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  beginnerCourses$!: Observable<Course[]>;
+  advancedCourses$!: Observable<Course[]>;
+
+  constructor(private coursesService: CoursesService) {}
+
+  ngOnInit(): void {
+    const courses$ = this.coursesService.findAllCourses();
+
+    this.beginnerCourses$ = courses$.pipe(map(courses => (courses ? courses.filter(course => course.category === 'BEGINNER') : [])));
+    this.advancedCourses$ = courses$.pipe(map(courses => (courses ? courses.filter(course => course.category === 'ADVANCED') : [])));
+  }
 }

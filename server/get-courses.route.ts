75488912fa2f9eb3077a01cbc11f5f4e48fb7553
victor <gfeng005@uottawa.ca>
2025-02-21 14:03:25 -0500

    added new api service to getCourseByID

--- server/get-courses.route.ts
+++ server/get-courses.route.ts
@@ -4,3 +4,14 @@ import {COURSES} from "./db-data";
 export function getAllCourses(req: Request, res: Response) {
     res.status(200).json({payload: Object.values(COURSES).sort((a: any, b: any) => a.seqNo - b.seqNo)})
 }
+
+export function getCourseById(req: Request, res: Response) {
+
+    const courseId = req.params["id"];
+
+    const courses: any = Object.values(COURSES);
+
+    const course = courses.find((course: any) => course.id == courseId);
+
+    res.status(200).json(course);
+}
--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 import { Application } from "express";
-import { getAllCourses } from "./get-courses.route";
+import { getAllCourses, getCourseById } from "./get-courses.route";
 
 const app: Application = express();
 
 app.route('/api/courses').get(getAllCourses);
+app.route('/api/courses/:id').get(getCourseById);
 
 const httpServer:any = app.listen(9000, () => {
     console.log("HTTP REST API Server running at http://localhost:" + httpServer.address().port);

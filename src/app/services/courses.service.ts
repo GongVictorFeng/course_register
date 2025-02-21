4c96262af7cd32234748571919e5f80d94df0c08
victor <gfeng005@uottawa.ca>
2025-02-21 17:03:16 -0500

    created two method in courses service to get lessons

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Course } from '../model/course';
+import { Lesson } from '../model/lesson';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +17,29 @@ export class CoursesService {
 
   findAllCourses(): Observable<Course[]> {
     return this.http.get<{ payload: Course[] }>('/api/courses').pipe(map(res => res['payload']));
+  }
+
+  findAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http.get<{ payload: Lesson[]}>('/api/lessons', {
+      params: new HttpParams()
+        .set('courseId', courseId.toString())
+        .set('pageNumber', '0')
+        .set('pageSize', '1000')
+    }).pipe(
+      map(res => res['payload'])
+    )
+  }
+
+  findLessons(courseId: number, sortOrder = 'asc', pageNumber = 0, pageSize = 3, sortColumn = 'seqNo'): Observable<Lesson[]> {
+    return this.http.get< {payload: Lesson[]} >('/api/lessons', {
+      params: new HttpParams()
+        .set('courseId', courseId.toString())
+        .set('sortOrder', sortOrder)
+        .set('pageNumber', pageNumber.toString())
+        .set('pageSize', pageSize.toString())
+        .set('sortColumn', sortColumn)
+    }).pipe(
+      map(res => res['payload'])
+    )
   }
 }

7b4dfd13567034acea03899a42deefcbdb7f0dda
victor <gfeng005@uottawa.ca>
2025-02-21 13:35:39 -0500

    added route for the btn view course in courses-card-list

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -3,13 +3,15 @@ import { Course } from '../model/course';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule} from '@angular/material/button'
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'courses-card-list',
   imports: [
     MatCardModule,
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule
   ],
   templateUrl: './courses-card-list.component.html',
   styleUrl: './courses-card-list.component.scss'

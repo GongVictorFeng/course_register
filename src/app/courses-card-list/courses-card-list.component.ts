c8c76e50e6e835ee927fbf832e88f11ee9f5d59e
victor <gfeng005@uottawa.ca>
2025-02-24 12:15:28 -0500

    added responsive screen for course card

--- src/app/courses-card-list/courses-card-list.component.ts
+++ src/app/courses-card-list/courses-card-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Course } from '../model/course';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
@@ -7,6 +7,8 @@ import { RouterModule } from '@angular/router';
 import { openEditCourseDialog } from '../course-dialog/course-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { filter } from 'rxjs';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'courses-card-list',
@@ -14,17 +16,54 @@ import { filter } from 'rxjs';
     MatCardModule,
     CommonModule,
     MatButtonModule,
-    RouterModule
+    RouterModule,
+    MatGridListModule
   ],
   templateUrl: './courses-card-list.component.html',
   styleUrl: './courses-card-list.component.scss'
 })
-export class CoursesCardListComponent {
+export class CoursesCardListComponent implements OnInit{
 
 
   @Input() courses!: Course[] | null;
 
-  constructor(private dialog: MatDialog) {}
+  cols = 3;
+  rowHeight = '500px';
+  handsetPortrait = false;
+
+  constructor(private dialog: MatDialog, private responsive: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.responsive.observe([
+      Breakpoints.TabletPortrait,
+      Breakpoints.TabletLandscape,
+      Breakpoints.HandsetPortrait,
+      Breakpoints.HandsetLandscape
+    ]).subscribe(
+      result => {
+        this.cols = 3;
+        this.rowHeight = "500px";
+        this.handsetPortrait = false;
+
+        const breakpoints = result.breakpoints;
+
+        if (breakpoints[Breakpoints.TabletPortrait]) {
+            this.cols = 1;
+        }
+        else if (breakpoints[Breakpoints.HandsetPortrait]) {
+            this.cols = 1;
+            this.rowHeight = "430px";
+            this.handsetPortrait = true;
+        }
+        else if (breakpoints[Breakpoints.HandsetLandscape]) {
+            this.cols = 1;
+        }
+        else if (breakpoints[Breakpoints.TabletLandscape]) {
+            this.cols = 2;
+        }
+      }
+    )
+  }
 
   editCourse(course: Course) {
     openEditCourseDialog(this.dialog, course)

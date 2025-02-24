cdb207f042866f7f19bac6928d48712a94112d07
victor <gfeng005@uottawa.ca>
2025-02-23 19:38:55 -0500

    created the course dialog component for editing course

---
+++ src/app/course-dialog/course-dialog.component.ts
@@ -0,0 +1,68 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {  MAT_DIALOG_DATA, MatDialog, MatDialogConfig, MatDialogModule, MatDialogRef } from "@angular/material/dialog";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { Course } from '../model/course';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'course-dialog',
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './course-dialog.component.html',
+  providers: [provideNativeDateAdapter()],
+  styleUrl: './course-dialog.component.scss'
+})
+export class CourseDialogComponent implements OnInit {
+
+  description: string = '';
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              @Inject(MAT_DIALOG_DATA) private course: Course,
+              private dialogRef: MatDialogRef<CourseDialogComponent>) {}
+
+  ngOnInit(): void {
+    this.description = this.course.description;
+
+    this.form = this.fb.group({
+      description: [this.course.description, Validators.required],
+      category: [this.course.category, Validators.required],
+      releasedAt: [new Date(), Validators.required],
+      longDescription: [this.course.longDescription, Validators.required]
+   });
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  save() {
+    this.dialogRef.close(this.form.value )
+  }
+
+}
+
+export function openEditCourseDialog(dialog: MatDialog, course: Course) {
+
+  const config = new MatDialogConfig();
+  config.disableClose = true;
+  config.autoFocus = true;
+
+  config.data = course;
+
+  const dialogRef = dialog.open(CourseDialogComponent, config);
+
+  return dialogRef.afterClosed();
+}

848aedafd3b014602ec695d80087000d527e93ce
victor <gfeng005@uottawa.ca>
2025-02-24 21:40:55 -0500

    added the starter for the tree-demo component

---
+++ src/app/tree-demo/tree-demo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-demo',
+  imports: [],
+  templateUrl: './tree-demo.component.html',
+  styleUrl: './tree-demo.component.scss'
+})
+export class TreeDemoComponent {
+
+}

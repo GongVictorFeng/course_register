bb70b98f9b6532162aa88702b85d15759c806dff
victor <gfeng005@uottawa.ca>
2025-02-26 16:08:02 -0500

    better to use variable isLoading for spinner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { CoursesCardListComponent } from '../courses-card-list/courses-card-list.component';
-import { map, Observable } from 'rxjs';
+import { finalize, map, Observable } from 'rxjs';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { CommonModule } from '@angular/common';
@@ -30,13 +30,18 @@ export class HomeComponent implements OnInit {
 
   beginnerCourses$!: Observable<Course[]>;
   advancedCourses$!: Observable<Course[]>;
+  isLoading = false;
 
   constructor(private coursesService: CoursesService) {}
 
   ngOnInit(): void {
+    this.isLoading = true;
     const courses$ = this.coursesService.findAllCourses();
 
-    this.beginnerCourses$ = courses$.pipe(map(courses => (courses ? courses.filter(course => course.category === 'BEGINNER') : [])));
+    this.beginnerCourses$ = courses$.pipe(
+      map(courses => (courses ? courses.filter(course => course.category === 'BEGINNER') : [])),
+      finalize(() => this.isLoading = false)
+    );
     this.advancedCourses$ = courses$.pipe(map(courses => (courses ? courses.filter(course => course.category === 'ADVANCED') : [])));
   }
 }

9e97d5657da3231555a0b1482dc44fd0877ca224
victor <gfeng005@uottawa.ca>
2025-02-26 00:09:45 -0500

    created a baseUrl variable

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -3,24 +3,27 @@ import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Course } from '../model/course';
 import { Lesson } from '../model/lesson';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private baseUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) { }
 
   findCourseById(courseId: number): Observable<Course> {
-    return this.http.get<Course>(`/api/courses/${courseId}`)
+    return this.http.get<Course>(`${this.baseUrl}/api/courses/${courseId}`)
   }
 
   findAllCourses(): Observable<Course[]> {
-    return this.http.get<{ payload: Course[] }>('/api/courses').pipe(map(res => res['payload']));
+    return this.http.get<{ payload: Course[] }>(`${this.baseUrl}/api/courses`).pipe(map(res => res['payload']));
   }
 
   findAllCourseLessons(courseId: number): Observable<Lesson[]> {
-    return this.http.get<{ payload: Lesson[]}>('/api/lessons', {
+    return this.http.get<{ payload: Lesson[]}>(`${this.baseUrl}/api/lessons`, {
       params: new HttpParams()
         .set('courseId', courseId.toString())
         .set('pageNumber', '0')
@@ -31,7 +34,7 @@ export class CoursesService {
   }
 
   findLessons(courseId: number, sortOrder = 'asc', pageNumber = 0, pageSize = 3, sortColumn = 'seqNo'): Observable<Lesson[]> {
-    return this.http.get< {payload: Lesson[]} >('/api/lessons', {
+    return this.http.get< {payload: Lesson[]} >(`${this.baseUrl}/api/lessons`, {
       params: new HttpParams()
         .set('courseId', courseId.toString())
         .set('sortOrder', sortOrder)

b93602c68f2f05dcf3fa87164e69029b5b626938
victor <gfeng005@uottawa.ca>
2025-03-11 12:39:48 -0400

    added the save course route to handle the request of save course.

---
+++ server/save-course.route.ts
@@ -0,0 +1,26 @@
+import {Request, Response} from 'express';
+import {COURSES} from "./db-data";
+import {setTimeout} from 'timers';
+
+export function saveCourse(req: Request, res: Response) {
+    /*
+        console.log("ERROR saving course!");
+        res.sendStatus(500);
+        return;
+    */
+   const courseId = req.params['id'];
+   const changes = req.body;
+   console.log(changes);
+   console.log("Saving course changes", courseId, JSON.stringify(changes));
+
+   const newCourse = {
+    ...COURSES[courseId],
+    ...changes
+   };
+   COURSES[courseId] = newCourse;
+   console.log("new course version", newCourse);
+
+   setTimeout(() => {
+    res.status(200).json(COURSES[courseId])
+   })
+}
--- server/server.ts
+++ server/server.ts
@@ -3,9 +3,12 @@ import { Application } from "express";
 import { getAllCourses, getCourseById } from "./get-courses.route";
 import { searchLessons } from './search-lessons.route';
 import * as cors from 'cors';
+import { saveCourse } from './save-course.route';
 // import * as path from 'path';
 
+const bodyParser = require('body-parser');
 const app: Application = express();
+app.use(bodyParser.json());
 app.use(cors());
 
 // // Serve static files from the dist directory
@@ -13,6 +16,7 @@ app.use(cors());
 
 app.route('/api/courses').get(getAllCourses);
 app.route('/api/courses/:id').get(getCourseById);
+app.route('/api/courses/:id').put(saveCourse);
 app.route('/api/lessons').get(searchLessons);
 
 const httpServer:any = app.listen(9000, () => {

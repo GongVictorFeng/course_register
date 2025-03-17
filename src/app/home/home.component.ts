fdb3cf4d598cfc95bdecee865439d6befa1a9e9b
victor <gfeng005@uottawa.ca>
2025-03-17 11:32:48 -0400

    integrated the course  store to the home component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,6 +12,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { LoadingService } from '../loading/loading.service';
 import { MessagesService } from '../messages/messages.service';
+import { CoursesStore } from '../services/courses.store';
 
 @Component({
   selector: 'app-home',
@@ -34,28 +35,15 @@ export class HomeComponent implements OnInit {
   advancedCourses$!: Observable<Course[]>;
 
   constructor(
-      private coursesService: CoursesService, 
-      private loadingService: LoadingService, 
-      private messagesService: MessagesService) {}
+      private coursesStore: CoursesStore) {}
 
   ngOnInit(): void {
     this.reloadCourses();
   }
 
   reloadCourses() {
-    const courses$ = this.coursesService.loadAllCourses().pipe(
-      map(courses => courses.sort(sortCoursesBySeqNo)),
-      catchError(err => {
-        const message = "Could not load courses";
-        this.messagesService.showErrors(message);
-        console.log(message, err);
-        return throwError(() => err);
-      })
-    );
-    const loadCourse$ = this.loadingService.showLoaderUntilCompleted(courses$)
-
-    this.beginnerCourses$ = loadCourse$.pipe(map(courses => (courses.filter(course => course.category === 'BEGINNER'))));
-    this.advancedCourses$ = loadCourse$.pipe(map(courses => (courses.filter(course => course.category === 'ADVANCED'))));
+    this.beginnerCourses$ = this.coursesStore.filterByCategory("BEGINNER");
+    this.advancedCourses$ = this.coursesStore.filterByCategory("ADVANCED");
   }
 }
 
